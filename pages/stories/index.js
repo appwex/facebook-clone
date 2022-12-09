d1bb633087a44275124b8b4f1bca2b3424459aeb
ranaintizar <intizaralirana2@gmail.com>
2022-12-10 00:28:14 +0500

    Add Button Component

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -4,8 +4,8 @@ import stl from './Stories.module.scss'
 const StoriesLinks = () => (
   <div className={stl.wrapper}>
     <h3>Stories of Components</h3>
-    <Link href="/stories/exampleComponent">Example Component</Link>
     <Link href="/stories/iconButton">Icon Button</Link>
+    <Link href="/stories/button">Button</Link>
   </div>
 )
 
