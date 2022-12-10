d82875427fed4a30de39746a56a5eda47b35fd17
ranaintizar <intizaralirana2@gmail.com>
2022-12-10 17:29:20 +0500

    Minor Fixes

--- pages/stories/menuItems.js
+++ pages/stories/menuItems.js
@@ -1,8 +1,12 @@
 import Stories from '../../src/components/stories'
 import MenuItems from '../../src/components/menuitems'
 
+import FriendsIcon from '../../src/assets/friendsIcon2.svg'
+
 const menuItems = {
   arg: 'Menu Items',
+  icon: <FriendsIcon />,
+  label: 'Friends',
 }
 
 const MenuItemsStory = () => (
