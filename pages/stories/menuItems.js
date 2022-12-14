548435b5dd0ea77de11ea00aba2e11ab071a11df
ranaintizar <intizaralirana2@gmail.com>
2022-12-15 00:26:20 +0500

    Minor Fix

--- pages/stories/menuItems.js
+++ pages/stories/menuItems.js
@@ -3,8 +3,6 @@ import MenuItems from 'components/menuitems'
 
 import FriendsIcon from 'assets/svg/friendsIcon2.svg'
 
-import FriendsIcon from '../../src/assets/friendsIcon2.svg'
-
 const menuItems = {
   arg: 'Menu Items',
   icon: <FriendsIcon />,
