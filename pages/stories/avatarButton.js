f4db51334a454ba91339aaf5c083e111ef6fcae5
ranaintizar <intizaralirana2@gmail.com>
2022-12-14 23:56:04 +0500

    Minor Fix

--- pages/stories/avatarButton.js
+++ pages/stories/avatarButton.js
@@ -3,11 +3,25 @@ import AvatarButton from 'components/avatarButton'
 
 const avatarButton = {
   arg: 'Avatar Button',
+  imgSrc: 'https://i.postimg.cc/xT7Y3ZxR/profile.jpg',
+  variant: 'noStatus',
+}
+
+const avatarButtonStatus = {
+  arg: 'Avatar Button Status',
+  imgSrc: 'https://i.postimg.cc/xT7Y3ZxR/profile.jpg',
+  variant: 'status',
+}
+
+const avatarButtonBordered = {
+  arg: 'Avatar Button Bordered',
+  imgSrc: 'https://i.postimg.cc/xT7Y3ZxR/profile.jpg',
+  variant: 'bordered',
 }
 
 const AvatarButtonStory = () => (
   <Stories
-    argList={[avatarButton]}
+    argList={[avatarButton, avatarButtonStatus, avatarButtonBordered]}
     template={(args) => <AvatarButton {...args} />}
   />
 )
